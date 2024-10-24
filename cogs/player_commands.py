c6bf5a0c27bd8cf401b6536ad4d0f99b22e3d9ab
= <txbbrand@gmail.com>
2024-10-24 19:41:53 -0400

    minor order change to error messages in vote/unvote

--- cogs/player_commands.py
+++ cogs/player_commands.py
@@ -89,57 +89,59 @@ class PlayerCommands(commands.Cog):
     )
     async def vote(self, ctx: discord.ApplicationContext, voted_for_name: str):
 
-        if ctx.channel.id in config.valid_channel_ids:
-            if config.majority:
-                await ctx.respond("Majority has been reached. Voting commands have been disabled.")
-                print("-i Majority reached, voting commands have been disabled")
-                return
-            
-            if not config.timer_on:
-                await ctx.respond("Time is up. Voting commands have been disabled.")
-                print("-i Time is up, voting commands have been disabled")
-                return
-            
-            username = ctx.user.name
-            if not db.is_playing(username):
-                await ctx.respond("You are not alive in this game!")
-                print(f"-i Non-participating player {ctx.user.name} cannot vote")
-                return
-            
-            initial_response = await ctx.respond("Sending your vote in...")
-            voter_name = db.get_name_from_username(username)
-            print(f"-> Sending in {voter_name}'s vote on {voted_for_name}...")
-            match(db.vote(username,voted_for_name)):
-                case -1:
-                    await initial_response.edit(content=f"You have already voted!")
-                    print(f"-i Player {ctx.user.name} has already voted")
-                case 2:
-                    await initial_response.edit(content=f"Player \'{voted_for_name}\' does not exist. Please check your spelling and try again.")
-                    print(f"-i Player'{voted_for_name}' does not exist")
-                case 1:
-                    await initial_response.edit(content=f"There was an unexpected error when processing vote for {voted_for_name}. Please try again.")
-                    print(f"-- An unexpected error occurred when sending in {ctx.user.name}'s vote for {voted_for_name}")
-                case 1000:
-                    config.timer_on = False
-                    config.majority = True
-                    await initial_response.edit(content=f"You voted for {voted_for_name}. **MAJORITY REACHED**")
-                    mod = await self.bot.fetch_user(config.mod_to_dm)
-                    await mod.send("A majority has been reached!")
-                    resp: discord.Message = await initial_response.original_response()
-                    for c in config.log_channel_ids:
-                        log_channel: discord.TextChannel = self.bot.get_channel(c)
-                        await log_channel.send(f"[(LINK TO MESSAGE)]({resp.jump_url}) {voter_name} voted for {voted_for_name}. **MAJORITY REACHED**")
-                    print(f"-+ {voter_name} voted for {voted_for_name} and a majority was reached")
-                case 0:
-                    await initial_response.edit(content=f"You voted for {voted_for_name}.")
-                    resp: discord.Message = await initial_response.original_response()
-                    for c in config.log_channel_ids:
-                        log_channel: discord.TextChannel = self.bot.get_channel(c)
-                        await log_channel.send(f"[(LINK TO MESSAGE)]({resp.jump_url}) {voter_name} voted for {voted_for_name}.")
-                    print(f"-+ {voter_name} voted for {voted_for_name}")
-        else:
+        if ctx.channel.id not in config.valid_channel_ids:
             await ctx.respond(content="Mafia commands are not allowed in this channel. Please ask an admin to use /setchannel or use the appropriate channels.")
             print("-i Vote sent in from a channel not flagged for voting commands, ignoring")
+            return
+
+        if config.majority:
+            await ctx.respond("Majority has been reached. Voting commands have been disabled.")
+            print("-i Majority reached, voting commands have been disabled")
+            return
+        
+        if not config.timer_on:
+            await ctx.respond("Time is up. Voting commands have been disabled.")
+            print("-i Time is up, voting commands have been disabled")
+            return
+        
+        username = ctx.user.name
+        if not db.is_playing(username):
+            await ctx.respond("You are not alive in this game!")
+            print(f"-i Non-participating player {ctx.user.name} cannot vote")
+            return
+        
+        initial_response = await ctx.respond("Sending your vote in...")
+        voter_name = db.get_name_from_username(username)
+        print(f"-> Sending in {voter_name}'s vote on {voted_for_name}...")
+        match(db.vote(username,voted_for_name)):
+            case -1:
+                await initial_response.edit(content=f"You have already voted!")
+                print(f"-i Player {ctx.user.name} has already voted")
+            case 2:
+                await initial_response.edit(content=f"Player \'{voted_for_name}\' does not exist. Please check your spelling and try again.")
+                print(f"-i Player'{voted_for_name}' does not exist")
+            case 1:
+                await initial_response.edit(content=f"There was an unexpected error when processing vote for {voted_for_name}. Please try again.")
+                print(f"-- An unexpected error occurred when sending in {ctx.user.name}'s vote for {voted_for_name}")
+            case 1000:
+                config.timer_on = False
+                config.majority = True
+                await initial_response.edit(content=f"You voted for {voted_for_name}. **MAJORITY REACHED**")
+                mod = await self.bot.fetch_user(config.mod_to_dm)
+                await mod.send("A majority has been reached!")
+                resp: discord.Message = await initial_response.original_response()
+                for c in config.log_channel_ids:
+                    log_channel: discord.TextChannel = self.bot.get_channel(c)
+                    await log_channel.send(f"[(LINK TO MESSAGE)]({resp.jump_url}) {voter_name} voted for {voted_for_name}. **MAJORITY REACHED**")
+                print(f"-+ {voter_name} voted for {voted_for_name} and a majority was reached")
+            case 0:
+                await initial_response.edit(content=f"You voted for {voted_for_name}.")
+                resp: discord.Message = await initial_response.original_response()
+                for c in config.log_channel_ids:
+                    log_channel: discord.TextChannel = self.bot.get_channel(c)
+                    await log_channel.send(f"[(LINK TO MESSAGE)]({resp.jump_url}) {voter_name} voted for {voted_for_name}.")
+                print(f"-+ {voter_name} voted for {voted_for_name}")
+            
 
     """ 
     /unvote
@@ -152,47 +154,46 @@ class PlayerCommands(commands.Cog):
         description="Revoke your vote on a player."
     )
     async def unvote(self, ctx: discord.ApplicationContext):
-        if ctx.channel.id in config.valid_channel_ids:
-            if config.majority:
-                await ctx.respond("Majority has been reached. Voting commands have been disabled.")
-                print("-i Majority reached, voting commands have been disabled")
-                return
-            
-            if not config.timer_on:
-                await ctx.respond("Time is up. Voting commands have been disabled.")
-                print("-i Time is up, voting commands have been disabled")
-                return
-            
-            username = ctx.user.name
-            if not db.is_playing(username):
-                await ctx.respond("You are not alive in this game!")
-                print(f"-i Non-participating player {ctx.user.name} cannot unvote")
-                return 
-            
-            initial_response = await ctx.respond("Striking thy name from the archives...")
-            unvoter_name = db.get_name_from_username(username)
-            print(f"-> {unvoter_name} is unvoting...")
-            match(db.unvote(username)):
-                case 1:
-                    print("-- An error occurred when processing unvote")
-                    await initial_response.edit(content="There was an unexpected error when processing your unvote. Please try again.")
-                case -1:
-                    print(f"-i {unvoter_name} has not voted")
-                    await initial_response.edit(content="You haven't voted for anyone yet.")
-                case 0:
-                    await initial_response.edit(content="You unvoted.")
-                    resp: discord.Message = await initial_response.original_response()
-                    for c in config.log_channel_ids:
-                        log_channel: discord.TextChannel = self.bot.get_channel(c)
-                        await log_channel.send(f"[(LINK TO MESSAGE)]({resp.jump_url}) {unvoter_name} unvoted.")
 
-                    print(f"-+ {unvoter_name} unvoted")
-        else:
-            await ctx.respond("Mafia commands are not allowed in this channel. Please ask an admin to use /setchannel or use the appropriate channels.")
-            print("-i Unvote sent in from a channel not flagged for voting commands, ignoring")
+        if ctx.channel.id not in config.valid_channel_ids:
+            await ctx.respond(content="Mafia commands are not allowed in this channel. Please ask an admin to use /setchannel or use the appropriate channels.")
+            print("-i Vote sent in from a channel not flagged for voting commands, ignoring")
+            return
 
+        if config.majority:
+            await ctx.respond("Majority has been reached. Voting commands have been disabled.")
+            print("-i Majority reached, voting commands have been disabled")
+            return
+        
+        if not config.timer_on:
+            await ctx.respond("Time is up. Voting commands have been disabled.")
+            print("-i Time is up, voting commands have been disabled")
+            return
+        
+        username = ctx.user.name
+        if not db.is_playing(username):
+            await ctx.respond("You are not alive in this game!")
+            print(f"-i Non-participating player {ctx.user.name} cannot unvote")
+            return 
+        
+        initial_response = await ctx.respond("Striking thy name from the archives...")
+        unvoter_name = db.get_name_from_username(username)
+        print(f"-> {unvoter_name} is unvoting...")
+        match(db.unvote(username)):
+            case 1:
+                print("-- An error occurred when processing unvote")
+                await initial_response.edit(content="There was an unexpected error when processing your unvote. Please try again.")
+            case -1:
+                print(f"-i {unvoter_name} has not voted")
+                await initial_response.edit(content="You haven't voted for anyone yet.")
+            case 0:
+                await initial_response.edit(content="You unvoted.")
+                resp: discord.Message = await initial_response.original_response()
+                for c in config.log_channel_ids:
+                    log_channel: discord.TextChannel = self.bot.get_channel(c)
+                    await log_channel.send(f"[(LINK TO MESSAGE)]({resp.jump_url}) {unvoter_name} unvoted.")
 
-
+                print(f"-+ {unvoter_name} unvoted")
 
 def setup(bot: discord.Bot): # this is called by Pycord to setup the cog
     bot.add_cog(PlayerCommands(bot)) # add the cog to the bot
